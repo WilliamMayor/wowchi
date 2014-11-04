20cc3776551cbae8e92e53d59b5a2cc698e4bb61
William Mayor <mail@williammayor.co.uk>
2014-11-04 23:36:33 +0000

    Just keeping frozen stuff

--- wowchi/static/js/ready.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('#queue').waypoint('sticky');
-    $('#exploring').waypoint('sticky');
-    ko.applyBindings(wowchi);
-    wowchi.load_from_storage();
-    message.init();
-});
