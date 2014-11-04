a00d88a8a0d6bae10b7faf76045f784021da1857
William Mayor <mail@williammayor.co.uk>
2014-11-04 23:44:57 +0000

    Ignore CNAME

--- freeze.py
+++ freeze.py
@@ -3,7 +3,7 @@ from wowchi import app, unminified_js
 
 app.config['ROLLBAR_ENV'] = 'production'
 app.config['FREEZER_DESTINATION'] = '../../gh-pages'
-app.config['FREEZER_DESTINATION_IGNORE'] = ['.git*']
+app.config['FREEZER_DESTINATION_IGNORE'] = ['.git*', 'CNAME']
 unminified_js._set_filters('rjsmin')
 freezer = Freezer(app)
 
